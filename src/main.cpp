d544af08910adc0cf8517edb0180cb0b9cb61661
Christian Deme <cdeme@stud.hs-heilbronn.de>
2023-05-31 08:16:43 +0200

    Create base for multitasking using both cores

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,39 @@
 #include "ESP32_LoRaWAN.h"
+#include "Arduino.h"
+
+UBaseType_t uxPriority = 1;
+uint32_t usStackDepth = 10000;
+void *pvParameters = NULL;
+TaskHandle_t *pvCreatedTask = NULL;
+
+void core1Task(void *parameter)
+{
+  while (true)
+  {
+    // Core 1 task logic here
+    vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay for 1 second
+  }
+}
+
+void core0Task(void *parameter)
+{
+  while (true)
+  {
+    // Core 0 task logic here
+    vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay for 1 second
+  }
+}
 
 void setup()
 {
   Serial.begin(115200);
+
+  xTaskCreatePinnedToCore(core1Task, "Core 1 Task", usStackDepth, pvParameters, uxPriority, pvCreatedTask, 1);
+  xTaskCreatePinnedToCore(core0Task, "Core 0 Task", usStackDepth, pvParameters, uxPriority, pvCreatedTask, 0);
 }
 
-void loop() {}
+void loop()
+{
+  // Empty loop as tasks are scheduled by FreeRTOS
+  vTaskDelay(1);
+}
